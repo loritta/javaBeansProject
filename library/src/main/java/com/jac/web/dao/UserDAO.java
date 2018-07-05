23f33273084e403a185c13cbb9945aa19946d4ff
Roman Shaiko <34403190-rshaiko@users.noreply.github.com>
2018-07-05 00:41:41 -0400

    UserDAO, merged after conflict

--- library/src/main/java/com/jac/web/dao/UserDAO.java
+++ library/src/main/java/com/jac/web/dao/UserDAO.java
@@ -1,15 +1,12 @@
 package com.jac.web.dao;
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import com.jac.web.controller.Globals;
+import java.sql.SQLException;
 import com.jac.web.model.User;
 
 public class UserDAO {
 
-	
-
-	
 	public User getUser(String username) {
 		User u = null;
 		try {
@@ -39,4 +36,28 @@ public class UserDAO {
 		}
 		return u;
 	}
+	
+public void addUser() throws ClassNotFoundException {
+	User user = new User();
+	try {
+		String query = "INSERT INTO users (username, password, firstName, lastName, phone, address, city, province, zip, roleID) "
+				+ "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		PreparedStatement st = Globals.db.getConnection().prepareStatement(query);
+		
+		st.setString(1, user.getUsername());
+		st.setString(2, user.getPassword());
+		st.setString(3, user.getFirstName());
+		st.setString(4, user.getLastName());
+		st.setString(5, user.getPhone());
+		st.setString(6, user.getAddress());
+		st.setString(7, user.getCity());
+		st.setString(8, user.getProvince());
+		st.setString(9, user.getZip());
+		st.setString(10, "2");
+		st.execute();
+		
+	} catch (SQLException e) {		
+		e.printStackTrace();
+	}
+}
 }
